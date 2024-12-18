839a84d0996cfcca1e12b34dbb9501d30f356964
Bartosz Starzyński <jestem_spoko@o2.pl>
2024-12-18 15:47:56 +0100

    Implementacja eliminacji Gaussa, dodanie testow

--- src/gauss.c
+++ src/gauss.c
@@ -1,4 +1,7 @@
 #include "gauss.h"
+#include <math.h>
+
+#define EPS 1e-12
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
@@ -6,11 +9,17 @@
  */
 
 int eliminate(Matrix *mat, Matrix *b){
-
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-	 */
-
-	return 0;
+	int n = mat-> r; // liczba wierszy (zakladam ze macierz jest kwadratowa)
+	for(int k=0;k<n;k++){
+		if(fabs(mat->data[k][k]) < EPS) return 1; // macierz osobliwa
+		for(int i=k+1;i<n;i++){
+			double factor = mat->data[i][k] / mat->data[k][k]; // wyznaczam czynnik
+			for(int j=k;j<n;j++){
+				mat->data[i][j] -= factor * mat->data[k][j];
+			}
+			b->data[i][0] -= factor * b->data[k][0];
+		}
+	}
+	return 0; // sukces
 }
 
