450496a213ad752b5dc8fad6032e1e82df047e91
Jakub Pietrala <j4ckpie@users.noreply.github.com>
2024-12-18 16:20:17 +0100

    Added pivot selection for Gauss

--- src/gauss.c
+++ src/gauss.c
@@ -10,9 +10,31 @@
 
 int eliminate(Matrix *mat, Matrix *b){
 	int n = mat-> r; // liczba wierszy (zakladam ze macierz jest kwadratowa)
-	for(int k=0;k<n;k++){
+	for(int k = 0; k < n; k++){
+		int pivot = k;
+	
+		// wyznaczenie elementu glownego
+		for(int i = k + 1; i < n; i++) {
+			if(fabs(mat->data[i][k]) > fabs(mat->data[pivot][k])) {
+				pivot = i;
+			}
+		}
+
+		// zamiana wierszy jesli konieczna
+		if(pivot != k) {
+			for(int j = 0; j < n; j++) {
+				double tmp = mat->data[k][j];
+				mat->data[k][j] = mat->data[pivot][j];
+				mat->data[pivot][j] = tmp;
+			}
+			double btmp = b->data[k][0];
+			b->data[k][0] = b->data[pivot][0];
+			b->data[pivot][0] = btmp;
+		}
+
 		if(fabs(mat->data[k][k]) < EPS) return 1; // macierz osobliwa
-		for(int i=k+1;i<n;i++){
+
+		for(int i = k + 1; i < n; i++){
 			double factor = mat->data[i][k] / mat->data[k][k]; // wyznaczam czynnik
 			for(int j=k;j<n;j++){
 				mat->data[i][j] -= factor * mat->data[k][j];
